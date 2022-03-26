0d2e178d291c311363d0f3fb726e87a02787edb1
Aldo Henriquez <henriquez.aldo@live.cl>
2022-03-26 11:06:50 -0300

    [Update] se deja lista la búsqueda del formulario y se muestra el pokemon buscado

--- src/hooks/usePokemon.js
+++ src/hooks/usePokemon.js
@@ -7,12 +7,14 @@ export const usePokemon = ({txt_busqueda}) => {
 
   useEffect(() => {    
       getPokemons(txt_busqueda);
-  }, [poke])
+  }, [txt_busqueda])
     
     const getPokemons = async(txt_busqueda) => { 
       console.log('antes de enviar a la api',txt_busqueda);         
-      const resp = await pokemonApi.post(`http://localhost:8080/api/buscarpokemon/?txt_busqueda=${ txt_busqueda }`);      
-      setPokes(resp.data.result);      
+      const resp = await pokemonApi.post(`http://localhost:8080/api/buscarpokemon/?txt_busqueda=${ txt_busqueda }`); 
+      
+      setPokes(resp.data.result);
+           
     }
      
     return { 
