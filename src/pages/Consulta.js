c47bfc6659dc97d7288c60b535a6ad855b3fd3f1
Aldo Henriquez <henriquez.aldo@live.cl>
2022-03-27 12:56:07 -0300

    [Update] cambios de norma en mayucula de const, ajuste visual en form de búsqueda

--- src/pages/Consulta.js
+++ src/pages/Consulta.js
@@ -26,8 +26,8 @@ export const Consulta = ({handleSearch}) => {
   return (    
     <>
      <form className='form' onSubmit={ handleSubmit }>
-      <div className="row col-lg-6 col-md-10 col-12">
-       <div className="input-group mb-3">
+      <div className="row text-center">
+       <div className="input-group mb-3 text-center">
         <input type="text" className="form-control" id="txt_busqueda" name="txt_busqueda" placeholder="Ingrese el nombre a buscar o número" aria-label="Recipient's username" aria-describedby="button-addon2" onChange={ handleChange } value={ busqueda.txt_busqueda }/>
         <button className="btn btn-success" type="submit">Buscar</button>  
        </div>
